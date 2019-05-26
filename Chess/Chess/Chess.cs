868c95ea9cce869129b9c84af1457b9cd83a15bb
SimonenkoAlex <alexsim99.99@mail.ru>
2019-05-26 11:42:56 +0600

    Algoritm. Programmirovaniye shakhmatnykh pravil igry. C#. 53/5000 Algorithm. Programming chess rules of the game. C #

---
+++ Chess/Chess/Chess.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chess
+{
+    // 1. Алгоритм. Программирование шахматных правил игры. C#. - https://www.youtube.com/watch?v=ZuIB4s-5b9g
+    // Время 2:10:36
+    public class Chess
+    {
+        public string fen { get; private set; }
+        Board board;
+        Moves moves;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="FEN"> Forsyth–Edwards Notation - стандартная нотация записи шахматных диаграмм </param>
+        public Chess(string fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1") 
+        {
+            this.fen = fen;
+            board = new Board(fen);
+            moves = new Moves(board);
+        }
+
+        Chess(Board board)
+        {
+            this.board = board;
+            this.fen = board.fen;
+            moves = new Moves(board);
+        }
+
+        public Chess Move(string move) // Pe2e4 Pe7e8Q
+        {
+            FigureMoving fm = new FigureMoving(move);
+            if (!moves.CanMove(fm)) return this;
+            Board nextBoard = board.Move(fm);
+            Chess nextChess = new Chess(nextBoard);
+            return nextChess;
+        }
+
+        public char GetFigureAt(int x, int y)
+        {
+            Square square = new Square(x, y);
+            Figure f = board.GetFigureAt(square);
+            return f == Figure.none ? '.' : (char)f;
+        }
+    }
+}
